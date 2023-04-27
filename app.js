ed44dfea341876aafca45021c2b27a54825efa55
noob-wale <wallacesheundah@gmail.com>
2023-04-27 13:36:31 +0300

    updated ReadMe.md

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let choices = ["rock", "paper", "scissors"]
+const choices = ["rock", "paper", "scissors"]
 
 function getComputerChoice () {
         let choice = Math.floor(Math.random() * choices.length);
