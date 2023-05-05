4692204b85b6ba6e190e02e9edffa435572587c6
noobwale <wallacesheundah@gmail.com>
2023-05-05 13:38:03 +0300

    updated app.js

--- app.js
+++ app.js
@@ -38,47 +38,88 @@ function scoreUpdate() {
 //COMPUTERCHOICES
 const choices = ["roCk", "paPer", "scisSors"];
 const getComputerChoice = () => Math.floor(Math.random() * choices.length);
+const computerSelection = choices[getComputerChoice()].toLowerCase();
+
+
 
 //PLAYROUND FUNCTION
 function playRound() {
-  const computerSelection = choices[getComputerChoice()].toLowerCase();
 
-  if (playerSelection == computerSelection) {
-    scoreBoard.textContent = 'Its A Tie';
-    console.log('its a tie');
-    // return "Its A Tie";
-  } else if (playerSelection == "rock" && computerSelection == "scissors") {
-    scoreBoard.textContent = 'You Win';
-    playerResult += 1;
-    console.log('you win');
-    // return "You Win";
-  } else if (computerSelection == "rock" && playerSelection == "scissors") {
-    scoreBoard.textContent = 'You Lose! Rock Crashes Scissors';
-    computerResult += 1;
-    console.log('you lose');
-    // return "You Lose! Rock Crashes Scissors";
-  } else if (playerSelection == "scissors" && computerSelection == "paper") {
-    scoreBoard.textContent = 'You Win';
-    playerResult += 1;
-    console.log('you win');
-    // return "You Win";
-  } else if (computerSelection == "scissors" && playerSelection == "paper") {
-    scoreBoard.textContent = 'You Lose! Scissors Cuts Paper';
-    computerResult += 1;
-    console.log('you lose');
-    // return "You Lose! Scissors Cuts Paper";
-  } else if (playerSelection == "paper" && computerSelection == "rock") {
-    scoreBoard.textContent = 'You Win';
-    playerResult += 1;
-    console.log('you win');
-    // return "You Win";
-  } else if (computerSelection == "paper" && playerSelection == "rock") {
-    scoreBoard.textContent = 'You Lose! Paper Covers Rock';
-    computerResult += 1;
-    console.log('you lose')
-    // return "You Lose! Paper Covers Rock";
+  switch (playerSelection, computerSelection) {
+    case playerSelection === computerSelection:
+      scoreBoard.textContent = 'Its A Tie';
+      console.log('its a tie'); 
+      break;
+    case playerSelection === "rock" && computerSelection === "scissors":
+      scoreBoard.textContent = 'You Win';
+      playerResult += 1;
+      console.log('you win');
+      break;
+    case playerSelection === "scissors" && computerSelection === "paper":
+      scoreBoard.textContent = 'You Win';
+      playerResult += 1;
+      console.log('you win');
+      break;
+    case playerSelection === "paper" && computerSelection === "rock":
+      scoreBoard.textContent = 'You Win';
+      playerResult += 1;
+      console.log('you win');
+      break;  
+    case computerSelection === "rock" && playerSelection === "scissors":
+      scoreBoard.textContent = 'You Lose! Rock Crashes Scissors';
+      computerResult += 1;
+      console.log('you lose');
+      break;
+    case computerSelection === "scissors" && playerSelection === "paper":
+      scoreBoard.textContent = 'You Lose! Scissors Cuts Paper';
+      computerResult += 1;
+      console.log('you lose');
+      break
+    case computerSelection === "paper" && playerSelection === "rock":
+      scoreBoard.textContent = 'You Lose! Paper Covers Rock';
+      computerResult += 1;
+      console.log('you lose');
+      break; 
   };
-};
+}; 
+
+
+  // if (playerSelection == computerSelection) {
+  //   scoreBoard.textContent = 'Its A Tie';
+  //   console.log('its a tie');
+  //   // return "Its A Tie";
+  // } else if (playerSelection == "rock" && computerSelection == "scissors") {
+  //   scoreBoard.textContent = 'You Win';
+  //   playerResult += 1;
+  //   console.log('you win');
+  //   // return "You Win";
+  // } else if (computerSelection == "rock" && playerSelection == "scissors") {
+  //   scoreBoard.textContent = 'You Lose! Rock Crashes Scissors';
+  //   computerResult += 1;
+  //   console.log('you lose');
+  //   // return "You Lose! Rock Crashes Scissors";
+  // } else if (playerSelection == "scissors" && computerSelection == "paper") {
+  //   scoreBoard.textContent = 'You Win';
+  //   playerResult += 1;
+  //   console.log('you win');
+  //   // return "You Win";
+  // } else if (computerSelection == "scissors" && playerSelection == "paper") {
+  //   scoreBoard.textContent = 'You Lose! Scissors Cuts Paper';
+  //   computerResult += 1;
+  //   console.log('you lose');
+  //   // return "You Lose! Scissors Cuts Paper";
+  // } else if (playerSelection == "paper" && computerSelection == "rock") {
+  //   scoreBoard.textContent = 'You Win';
+  //   playerResult += 1;
+  //   console.log('you win');
+  //   // return "You Win";
+  // } else if (computerSelection == "paper" && playerSelection == "rock") {
+  //   scoreBoard.textContent = 'You Lose! Paper Covers Rock';
+  //   computerResult += 1;
+  //   console.log('you lose')
+  //   // return "You Lose! Paper Covers Rock";
+  // };
+
 
 // function game() {
 //   let i = 0;
